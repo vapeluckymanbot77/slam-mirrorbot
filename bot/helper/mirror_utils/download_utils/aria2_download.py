7e26b418e9e04251cc531d160fc1f8232260848a
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-04-13 23:03:58 +0530

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -70,7 +70,7 @@ class AriaDownloadHelper(DownloadHelper):
     def __onDownloadStopped(self, api, gid):
         LOGGER.info(f"onDownloadStop: {gid}")
         dl = getDownloadByGid(gid)
-        if dl: dl.getListener().onDownloadError('Dead torrent!')
+        if dl: dl.getListener().onDownloadError('Your Torrent Link is Dead or Seedless')
  
     @new_thread
     def __onDownloadError(self, api, gid):
